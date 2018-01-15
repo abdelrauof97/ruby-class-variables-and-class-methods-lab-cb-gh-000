51a5878a0ca0c83a6aebb84e0d631cd1133be56a
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-15 13:05:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,14 @@ class Song
   attr_reader :name, :artist, :genre 
 
   @@count = 0
-
+  @@genres = []
+  
+  
   def initialize(name,artist,genre)
     @name = name 
     @artist = artist
     @genre = genre
+    @@genres << genre unless @@genres.include?(genre)
     @@count += 1
   end
   
