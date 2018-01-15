a2b7040b6746d9cf5e77b867f5861e813a3a3cae
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-15 12:54:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,11 @@
 class Song 
   
-  attr_accessor 
+  attr_accessor :name, :artist, :genre 
   
-  def initialize()
-    
+  def initialize(name,artist,genre)
+    @@name = name 
+    @@artist = artist
+    @@genre = genre
   end
   
 end
